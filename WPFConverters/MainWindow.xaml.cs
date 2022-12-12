afa4e852e7a019f2c062b0a6c78a604474979890
Samuel Jaja <samuel.jaja21@gmail.com>
2022-12-12 06:03:16 +0100

    Added Converters as a Project

---
+++ WPFConverters/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFConverters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool CypherCrescentIdCard { get; set; } = false;
+        public bool CypherCrescentNigeria { get; set; } = true;
+        public bool HasThickBorder { get; set; } = true;
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
